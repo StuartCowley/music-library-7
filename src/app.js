f1342c7dec9a735acb0a9f22882c704866dab112
Ed Adkins <ejmadkins@gmail.com>
2022-04-28 20:45:16 +0100

    add artists

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
+const artistRouter = require("./routes/artist");
+
 const app = express();
 app.use(express.json());
 module.exports = app;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/artist", artistRouter);
